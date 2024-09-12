4c56c9001e1ccd6051afe19515806fdbb112aab5
Sas <ssa171@post.uit.no>
2024-09-12 13:53:55 +0200

    code

--- MVC/SimpleMVC/SimpleMVC/Models/Entities/Car.cs
+++ MVC/SimpleMVC/SimpleMVC/Models/Entities/Car.cs
@@ -2,5 +2,8 @@
 
 public class Car
 {
-    
+    public string CarId { get; set; } = "No license";
+    public string Make { get; set; } = "Unknown";
+    public string Model { get; set; } = "Unknown";
+    public int Year { get; set; }
 }
