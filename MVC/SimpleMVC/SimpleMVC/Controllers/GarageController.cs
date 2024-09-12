60def5c3e69cc9219f0a1f3406c60cd80a93548f
Sas <ssa171@post.uit.no>
2024-09-12 14:56:59 +0200

    update view garage

--- MVC/SimpleMVC/SimpleMVC/Controllers/GarageController.cs
+++ MVC/SimpleMVC/SimpleMVC/Controllers/GarageController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMVC.Models.Entities;
 
 namespace SimpleMVC.Controllers;
 
 public class GarageController : Controller
 {
-    // GET
+    private readonly List<Car> _cars = new()
+    {
+        new Car { CarId = "AX32968", Make = "Chevrolet", Model = "Camaro", Year = 1981},
+        new Car { CarId = "HF27343", Make = "Mazda", Model = "Mazda 6", Year = 2016},
+        new Car { CarId = "YZ97000", Make = "Hyundai", Model = "Santa Fe", Year = 2007}
+    };
+    
+    
+    // GET:
+    // Url: /Garage
+    // or simply "/" when default in program.cs is set to GarageController
     public IActionResult Index()
     {
-         return View();
+        return View(_cars);
     }
 }
