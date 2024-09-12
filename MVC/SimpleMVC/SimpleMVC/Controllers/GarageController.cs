aa3f4cc2189e8ce1cd2eb2eba2c3fb681062e57b
Sas <ssa171@post.uit.no>
2024-09-12 13:50:15 +0200

    Added controller
    added garage controller to controllers

---
+++ MVC/SimpleMVC/SimpleMVC/Controllers/GarageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleMVC.Controllers;
+
+public class GarageController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+         return View();
+    }
+}
