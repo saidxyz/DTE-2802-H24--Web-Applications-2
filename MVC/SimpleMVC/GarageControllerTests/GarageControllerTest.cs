29b7135438c390f314acdded248f0c0c2fa7fab5
Sas <ssa171@post.uit.no>
2024-09-14 15:26:34 +0200

    Unit test

--- MVC/SimpleMVC/GarageControllerTests/GarageControllerTest.cs
+++ MVC/SimpleMVC/GarageControllerTests/GarageControllerTest.cs
@@ -1,20 +1,37 @@
-﻿namespace GarageControllerTests;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimpleMVC.Controllers;
+using SimpleMVC.Models.Entities;
+using SimpleMVC.Repository;
+
+namespace GarageControllerTests;
 
 public class GarageControllerTest
 {
-    private readonly GarageController _garageController;
+    private readonly Mock<ICarRepository> _mockRepo;
+    private readonly GarageController _controller;
+
+    public GarageControllerTest()
+    {
+        _mockRepo = new Mock<ICarRepository>();
+        _controller = new GarageController(_mockRepo.Object);
+    }
     
-    
+    // Index 
     [Fact]
     public void Index_ReturnsAViewResult_WithAListOfCars()
     {
-        // Arrange
-        
-        
-        // Act
-        
-        
-        // Assert
-        
+        // Arrange: Works as a setup
+        var cars = new List<Car> {new Car(), new Car(), new Car(), new Car()};
+        _mockRepo.Setup(repo => repo.GetAll()).Returns(cars);
+
+        // Act: call the method from the controller, and keeps the result
+        var result = _controller.Index();
+
+        // Assert: Tests the results with expected result
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<List<Car>>(viewResult.ViewData.Model);
+        Assert.Equal(4, model.Count);
     }
 }
+    
