b96257eeaadc0fbb2f6ecf7ba34f2c9bb583b936
Sas <ssa171@post.uit.no>
2024-09-14 11:04:26 +0200

    seedData

--- MvcMovie/MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
 
+namespace MvcMovie.Data
+{
     public class MvcMovieContext : DbContext
     {
-        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
             : base(options)
         {
         }
 
         public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
     }
+}
