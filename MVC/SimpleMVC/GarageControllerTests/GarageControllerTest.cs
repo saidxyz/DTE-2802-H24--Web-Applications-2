1e6372c73889569db31fff4edf82d050b8b78f81
Sas <ssa171@post.uit.no>
2024-09-14 08:45:52 +0200

    update mvcMovie

---
+++ MVC/SimpleMVC/GarageControllerTests/GarageControllerTest.cs
@@ -0,0 +1,20 @@
+﻿namespace GarageControllerTests;
+
+public class GarageControllerTest
+{
+    private readonly GarageController _garageController;
+    
+    
+    [Fact]
+    public void Index_ReturnsAViewResult_WithAListOfCars()
+    {
+        // Arrange
+        
+        
+        // Act
+        
+        
+        // Assert
+        
+    }
+}
