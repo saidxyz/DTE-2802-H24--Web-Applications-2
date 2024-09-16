313f3d8d15dd83bc71f4aee0a6193bb20db713f8
Sas <ssa171@post.uit.no>
2024-09-16 13:46:56 +0200

    Update

--- MVC ProductStore/MVC ProductStore/Controllers/ProductController.cs
+++ MVC ProductStore/MVC ProductStore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ProductStore.Models.Entities;
 
 namespace MVC_ProductStore.Controllers;
 
@@ -7,6 +8,22 @@ public class ProductController : Controller
     // GET
     public IActionResult Index()
     {
+        List<Product> products = new List<Product>{
+            new Product {Name="Hammer", Price=121.50m, Category="Verktøy"},
+            new Product {Name="Vinkelsliper", Price=1520.00m, Category ="Verktøy"},
+            new Product {Name="Melk", Price=14.50m, Category ="Dagligvarer"},
+            new Product {Name="Kjøttkaker", Price=32.00m, Category ="Dagligvarer"},
+            new Product {Name="Brød", Price=25.50m, Category ="Dagligvarer"}
+        };
+        return View(products);
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
         return View();
     }
 }
+
