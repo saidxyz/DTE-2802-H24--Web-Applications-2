40561bf46db7d7828d0aee2067d95b2f385291e3
Sas <ssa171@post.uit.no>
2024-09-16 14:06:13 +0200

    Update

---
+++ MVC ProductStore/MVC ProductStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_ProductStore.Models.Entities; // Pass på at dette er riktig namespace for Product-klassen
+
+namespace MVC_ProductStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
