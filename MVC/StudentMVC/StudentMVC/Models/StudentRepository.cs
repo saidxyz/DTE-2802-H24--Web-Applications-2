aad8cd599628aed8ab0f9795f84594ab2cfabfd3
Sas <ssa171@post.uit.no>
2024-09-18 17:34:26 +0200

    update

---
+++ MVC/StudentMVC/StudentMVC/Models/StudentRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.Pkcs;
+using Microsoft.EntityFrameworkCore;
+using StudentMVC.Data;
+using StudentMVC.Models.Entities;
+
+namespace StudentMVC.Models;
+
+public class StudentRepository : IStudentRepository
+{
+    private static List<Student> Students { get; }
+    private readonly StudentDbContext _db;
+
+    public StudentRepository(StudentDbContext db)
+    {
+        _db = db;
+    }
+    
+    public IEnumerable<Student> GetAll()
+    {
+        var students = _db.Students
+            .Include(deg => deg.Degree)
+            .ToList();
+        return students;
+    }
+
+    public void Save(Student student)
+
+    {
+        // Add Student to database
+        // SAve changes
+    }
+}
