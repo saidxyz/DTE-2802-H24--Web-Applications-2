2d31f7bed2c630b3249f4cb3adb306fb0b5ffc14
Sas <ssa171@post.uit.no>
2024-09-18 13:32:59 +0200

    UnitTest

--- MVC/SimpleMVC/GarageControllerTests/GarageControllerTest.cs
+++ MVC/SimpleMVC/GarageControllerTests/GarageControllerTest.cs
@@ -8,12 +8,16 @@ namespace GarageControllerTests;
 
 public class GarageControllerTest
 {
+    // Lager moq av iCarRepository
+    // Velger selv data og den skal gi ut
     private readonly Mock<ICarRepository> _mockRepo;
     private readonly GarageController _controller;
 
     public GarageControllerTest()
     {
-        _mockRepo = new Mock<ICarRepository>();
+        // legger dette inn i en controller
+        // Fordi controller bruker repository
+        _mockRepo = new Mock<ICarRepository>(); 
         _controller = new GarageController(_mockRepo.Object);
     }
     
@@ -22,13 +26,16 @@ public class GarageControllerTest
     public void Index_ReturnsAViewResult_WithAListOfCars()
     {
         // Arrange: Works as a setup
-        var cars = new List<Car> {new Car(), new Car(), new Car(), new Car()};
+        // Lager liste med biler
+        var cars = new List<Car> {new Car(), new Car(), new Car(), new Car() };
+        // Fra iCarRepository, bruker vi metoden GetAll() og returnerer cars
         _mockRepo.Setup(repo => repo.GetAll()).Returns(cars);
 
         // Act: call the method from the controller, and keeps the result
         var result = _controller.Index();
 
         // Assert: Tests the results with expected result
+        // Sjekker at den er av rett type
         var viewResult = Assert.IsType<ViewResult>(result);
         var model = Assert.IsAssignableFrom<List<Car>>(viewResult.ViewData.Model);
         Assert.Equal(4, model.Count);
